28d67125fb0a9a62c9b747504eb897bfbcca2791
Berk San <san.berk8@gmail.com>
2023-02-22 20:47:56 +0300

    1. Delete SceneDelegate 2. Remove main.storyboard file 3. Group project files and folders based on MVVM

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Seafood-Recipes-MVVM
+
+## First Commit
+Create Readme.md
+
+## Second Commit
+1. Delete SceneDelegate
+2. Remove main.storyboard file
+3. Group project files and folders based on MVVM
---
+++ Seafood Recipes MVVM/Views/MealListVC.swift
@@ -0,0 +1,15 @@
+//
+//  MealListVC.swift
+//  Seafood Recipes MVVM
+//
+//  Created by Berk on 22.02.2023.
+//
+
+import UIKit
+
+class MealListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
